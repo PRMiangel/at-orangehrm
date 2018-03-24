1a3b43cfe34ae77388519b6b2af5a91495739b14
Miguel Pari Angel Pari <miangel-pr@outlook.com>
2018-03-23 23:51:02 -0400

    Create scenario to add an employee

---
+++ at-orangehrm/AT.OrangeHRM.UI.Spec/Features/PIM/AddEmployee.feature
@@ -0,0 +1,20 @@
+﻿@PIM
+Feature: Add Employee
+	As a Orange HRM user
+	I want to be able to add a new employee
+	So I can have all employees registered.
+
+@CRUD @BVT
+Scenario: Add a new Employee with First, Middle and Last Name fields
+	Given I navigate to PIM > Add Employee
+	When I Fill the following values
+		| Field       | Value      |
+		| First Name  | Purple     |
+		| Middle Name | Hrm        |
+		| Last Name   | Automation |
+		And I click on Save button
+		And I navigate to PIM > Employee List
+		And I fill 'Purple Hrm Automation' in Employee Name field
+		And I click on Search button
+	Then I should see 'Purple Hrm' in the table
+		And I should see 'Automation' in the table
---
+++ at-orangehrm/AT.OrangeHRM.UI.Spec/Support/UIHooks.cs
@@ -0,0 +1,72 @@
+﻿namespace AT.OrangeHRM.UI.Spec.Support
+{
+    using AT.OrangeHRM.UI.StepDefinitions.Commons;
+    using AT.OrangeHRM.UI.StepDefinitions.Steps;
+    using AT.OrangeHRM.UI.StepDefinitions.Steps.auth;
+    using TechTalk.SpecFlow;
+
+    /// <summary>
+    /// Contains the UI hooks.
+    /// </summary>
+    [Binding]
+    public sealed class UIHooks : BaseUISteps
+    {
+        /// <summary>
+        /// The constructor for <see cref="UIHooks"/>.
+        /// </summary>
+        /// <param name="scenarioContext">The scenario context.</param>
+        /// <param name="featureContext">The feature context.</param>
+        public UIHooks(ScenarioContext scenarioContext, FeatureContext featureContext) :
+            base(scenarioContext, featureContext)
+        {
+        }
+
+        /// <summary>
+        /// Opens the web browser.
+        /// </summary>
+        /// <param name="featureContext"></param>
+        [BeforeScenario(Order = 10)]
+        public static void OpenBrowser(FeatureContext featureContext)
+        {
+            var webBrowser = new WebBrowserSteps();
+            featureContext[Keys.WebBrowser] = webBrowser;
+            webBrowser.OpenWebBrowser("chrome");
+        }
+
+        /// <summary>
+        /// Go to Orange HRM.
+        /// </summary>
+        /// <param name="featureContext"></param>
+        [BeforeScenario(Order = 10)]
+        public static void GotoOrangeHRM(FeatureContext featureContext)
+        {
+            ((WebBrowserSteps)featureContext[Keys.WebBrowser]).GoTo("http://opensource.demo.orangehrmlive.com");
+        }
+
+        /// <summary>
+        /// Login into Orange HRM.
+        /// </summary>
+        [BeforeScenario]
+        public void LoginIntoOrangeHRM()
+        {
+            var loginSteps = new LoginSteps(ScenarioContext, FeatureContext);
+
+            loginSteps.LoginIntoOrangeHRM("Admin", "admin");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+        }
+
+        /// <summary>
+        /// Closes the browser.
+        /// </summary>
+        /// <param name="featureContext"></param>
+        [AfterFeature]
+        public static void CloseBrowser(FeatureContext featureContext)
+        {
+            ((WebBrowserSteps)featureContext[Keys.WebBrowser]).CloseWebBrowser();
+        }
+    }
+}
